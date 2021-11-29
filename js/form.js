ff4f78873778b03a450dd3a5b6dc8ccd9d7cacb6
jupex121 <89226846+jupex121@users.noreply.github.com>
2021-11-29 17:43:44 -0600

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,57 @@
+class Form{
+    constructor(){
+       this.input = createInput("");
+       this.button = createButton('Play');
+       this.greeting = createElement('h2');
+       this.title = createElement('h2');
+       this.reset = createButton('Reset');
+    }
+    hide() {
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+        this.title.hide();
+    }
+    display() {
+        this.title.html("FRUIT CATCHER");
+        this.title.position(575, 50);
+        this.title.style('font-size', '70px');
+        this.title.style('color', 'skyblue');
+        this.input.position(displayWidth / 2 +75, 400);
+        this.input.style('width', '200px');
+        this.input.style('height', '20px');
+        this.input.style('background', 'lavender');
+        this.button.position(displayWidth / 2 +78, 500);
+        this.button.style('width', '200px');
+        this.button.style('height', '40px');
+        this.button.style('background', 'lightpink');
+        this.reset.position(displayWidth / 2 +700, displayHeight / 2 +350);
+        this.reset.style('width', '100px');
+        this.reset.style('height', '30px');
+        this.reset.style('background', 'lightpink');
+
+        this.button.mousePressed(() => {
+            this.input.hide();
+            this.button.hide();
+            player.name = this.input.value();
+            playerCount += 1;
+            player.index = playerCount;
+            player.update();
+            player.updateCount(playerCount);
+            this.greeting.html("Hello " + player.name)
+            this.greeting.position(displayWidth / 2 +50, 250);
+            this.greeting.style('color', 'white');
+            this.greeting.style('font-size', '50px');
+        });
+
+        this.reset.mousePressed(() => {
+            //add code to reset the values of the gameState and the playerCount nodes to 0 in the database
+            game.update(0);
+            player.updateCount(0);
+            var playerInfoRef = database.ref('players');
+            playerInfoRef.remove();
+            
+        });
+
+    }
+}
